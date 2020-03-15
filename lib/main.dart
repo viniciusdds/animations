b4846f3eb013b52362f850248b60b165278e7a5f
viniciusdds <viniciusdamasceno180@hotmail.com>
2020-03-15 10:57:06 -0300

    primeira commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:animations/screens/home/home_screen.dart';
+import 'package:animations/screens/login/login_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: LoginScreen(),
+    );
+  }
+}
+
+
+
+
+
